af3073a49dcc0701c2ee2c209708eeeb3144f7b8
Chrisp1993 <chrispollio93@gmail.com>
2022-08-06 13:01:27 -0400

    added the for loop to get random elements from the usable array, added the writePassword function and the event listener

--- assets/script.js
+++ assets/script.js
@@ -6,37 +6,40 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 //Button//
 var generateBtn = document.querySelector("#generate");
+var finalPassword = "";
 
 //Function & if statements to generate password//
 function generatePassword() {
     var length = prompt("Enter Number of characters between 8-128.");
     console.log (length);
-    var finalPassword = ""
+    
+    console.log("finalPassword", finalPassword);
 }
 
     if (length < 8 || length > 128) {
       alert("Choose a number between 8 and 128");
-      return (" ")
     }
 
     if(isNaN(length)){
       alert("Choose a number between 8 and 128 then click the generate password button.");
-      return (" ")
     }
 
     //Variables to confirm characters//
     var isLowercase = confirm("Do you want to use lower case letters? Click 'OK' for Yes and 'Cancel' for No.");
+
     var isUppercase = confirm("Do you want to use upper case letters? Click 'OK' for Yes and 'Cancel' for No.");
+
     var isANumber = confirm("Do you want to use a number? Click 'OK' for Yes and 'Cancel' for No.");
+
     var isSpecialChar = confirm("Do you want to use a special character? Click 'OK' for Yes and 'Cancel' for No.");
 
     //Empty array for all usableChars concatinated//
-    var usableChars = []
+    var usableChars = [];
 
     if (isLowercase){
         finalPassword +=
            lowerCase[Math.floor(Math.random() * lowerCase.lenth)];
-           console.log(finalPassword)
+           console.log(finalPassword);
         usableChars = usableChars.concat(lowerCase);
     }
 
@@ -51,12 +54,38 @@ function generatePassword() {
         finalPassword +=
             numbers[Math.floor(Math.random() * numbers.length)];
             console.log(finalPassword);
-        usableChars = usableChars.concat(numbers)
+        usableChars = usableChars.concat(numbers);
     }
 
     if (isSpecialChar){
         finalPassword +=
             specialCh[Math.floor(Math.random() * specialCh.length)];
             console.log(finalPassword);
-        usableChars = usableChars.concat(specialCh)
+        usableChars = usableChars.concat(specialCh);
     }
+
+    //Confirmation minimum 1 character chosen//
+  if (!isLowercase && !isUppercase && !isANumber && !isSpecialChar) {
+    alert(
+      "Please choose at least one character option! Click GENERATE PASSWORD again."
+    );
+  }
+
+  //for loop to get random elements from usableChars array//
+  for (var i = finalPassword.length; i < length; i++) {
+    finalPassword +=
+      usableChars[Math.floor(Math.random() * usableChars.length)];
+  }
+  return finalPassword;
+}
+
+//Write password to the #password//
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+//Event listener to generate button//
+generateBtn.addEventListener("click", writePassword);
